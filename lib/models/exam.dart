f4fd31bee1df9729a152a6a8912c62763bf03aea
Hristina Zdraveska <kikezdraveska@gmail.com>
2025-11-09 22:49:43 +0100

    fix

--- lib/models/exam.dart
+++ lib/models/exam.dart
@@ -14,57 +14,57 @@ class Exam {
     final List<Exam> exams = [
       Exam(
         name: 'Математика 1',
-        dateTime: DateTime.now().subtract(const Duration(days: 5, hours: 2)),
+        dateTime: DateTime(2025, 12, 1, 9, 0),
         rooms: ['200АБ', '117'],
       ),
       Exam(
         name: 'Математика 2',
-        dateTime: DateTime.now().add(const Duration(days: 2, hours: 3)),
+        dateTime: DateTime(2025, 10, 13, 8, 0),
         rooms: ['138'],
       ),
       Exam(
         name: 'Оперативни системи',
-        dateTime: DateTime.now().add(const Duration(days: 10, hours: 1)),
+        dateTime: DateTime(2025, 11, 11, 11, 0),
         rooms: ['12', '13'],
       ),
       Exam(
         name: 'Компјутерски мрежи',
-        dateTime: DateTime.now().subtract(const Duration(days: 1, hours: 4)),
+        dateTime: DateTime(2025, 10, 11, 14, 30),
         rooms: ['2'],
       ),
       Exam(
         name: 'Мобилни информациски системи',
-        dateTime: DateTime.now().add(const Duration(days: 7)),
+        dateTime: DateTime(2025, 09, 01, 19, 0),
         rooms: ['3'],
       ),
       Exam(
         name: 'Дистрибуирани системи',
-        dateTime: DateTime.now().add(const Duration(days: 15, hours: 2)),
+        dateTime: DateTime(2025, 11, 18, 8, 0),
         rooms: ['200В', '215'],
       ),
       Exam(
         name: 'Тимски проект',
-        dateTime: DateTime.now().subtract(const Duration(days: 10)),
+        dateTime: DateTime(2025, 10, 29, 12, 0),
         rooms: ['215'],
       ),
       Exam(
         name: 'Професионални вештини',
-        dateTime: DateTime.now().add(const Duration(days: 20, hours: 5)),
+        dateTime: DateTime(2025, 12, 11, 13, 0),
         rooms: ['117'],
       ),
       Exam(
         name: 'Алгоритми и податочни структури',
-        dateTime: DateTime.now().add(const Duration(days: 1, hours: 1)),
+        dateTime: DateTime(2025, 12, 05, 15, 0),
         rooms: ['12', '13'],
       ),
       Exam(
         name: 'Структурно програмирање',
-        dateTime: DateTime.now().subtract(const Duration(days: 3)),
+        dateTime: DateTime(2025, 11, 11, 10, 0),
         rooms: ['200АБ'],
       ),
       Exam(
         name: 'Економија за ИКТ инженери',
-        dateTime: DateTime.now().add(const Duration(days: 12, hours: 4)),
+        dateTime: DateTime(2025, 12, 17, 17, 0),
         rooms: ['200В'],
       ),
     ];
